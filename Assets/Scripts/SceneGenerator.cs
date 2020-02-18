14cc4f582564cfa858954585ddbee4dcdb238b1c
David_Goru <david.gomez.rooiz@gmail.com>
2020-02-18 14:43:11 +0100

    First commit

---
+++ Assets/Scripts/SceneGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneGenerator : MonoBehaviour
+{
+    Vector3 roomPos;
+    Quaternion roomRot;
+
+    void Start()
+    {
+        GenerateRooms();
+    }
+
+    void GenerateRooms()
+    {
+        // Test rooms
+        GameObject baseRoom1 = Resources.Load<GameObject>("Rooms/Base/Base room 1");
+        GameObject baseRoom2 = Resources.Load<GameObject>("Rooms/Base/Base room 2");
+
+        roomRot = Quaternion.Euler(0, 45, 0);
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                roomPos = new Vector3(((j % 2 == 0) ? 2.825f : 0) + i * 5.65f, j * 4, -j * 2.8f);
+
+                Instantiate((Random.Range(0, 2) >= 1) ? baseRoom1 : baseRoom2, roomPos, roomRot);
+                
+                
+                //Instantiate((Random.Range(0, 2) >= 1) ? baseRoom1 : baseRoom2, new Vector3(((j % 2 == 0) ? 2.825f : 0) + i * 5.65f, j * 4, -j * 2.8f), Quaternion.Euler(0, 0, 0));
+            }
+        }
+    }
+}
